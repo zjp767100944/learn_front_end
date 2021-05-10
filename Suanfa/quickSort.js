eb25cac0b3aa4159fea637f8b4ef9d156af83248
张俊鹏 <zhangjunpeng.l1n@bytedance.com>
2021-05-11 00:35:01 +0800

    feat: 快速排序

--- Suanfa/quickSort.js
+++ Suanfa/quickSort.js
@@ -1,11 +1,28 @@
-function quickSort(arr) {
-  var l =
-  
-    function sort(arr, l, r) {
-      
-      while(l < r && arr[l] < arr[r]) l++;
-      while(l < r && arr[r] > arr[l]) r-- 
-
-    }
-  
+function handleQuickSort(arr, l, r) {
+  if (l < r) {
+    let p = part(arr, l, r);
+    handleQuickSort(arr, l, p - 1);
+    handleQuickSort(arr, p + 1, r);
+  }
 }
+
+function part(arr, l, r) {
+  let o = arr[l];
+  while (l < r) {
+    while (o <= arr[r] && l < r) {
+      r--;
+    }
+    arr[l] = arr[r];
+    while (o > arr[l] && l < r) {
+      l++;
+    }
+    arr[r] = arr[l];
+  }
+  arr[l] = o;
+  return l;
+}
+
+const quickSort = arr => handleQuickSort(arr, 0, arr.length - 1);
+const arr = [2, 1, 5, 7, 2, 4, 9, 8];
+quickSort(arr);
+console.log(arr);
