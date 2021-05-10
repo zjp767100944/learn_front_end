e8224c0d125e0ecdcfae2e835b382ccf608cabed
张俊鹏 <zhangjunpeng.l1n@bytedance.com>
2021-05-10 23:57:59 +0800

    feat:  init

---
+++ Suanfa/quickSort.js
@@ -0,0 +1,11 @@
+function quickSort(arr) {
+  var l =
+  
+    function sort(arr, l, r) {
+      
+      while(l < r && arr[l] < arr[r]) l++;
+      while(l < r && arr[r] > arr[l]) r-- 
+
+    }
+  
+}
